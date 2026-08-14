54d2364ee3d132410915a8907a2f7c4fa41c48ff
ELMartin31611 <martinborja31611@gmail.com>
2026-08-14 17:42:47 -0500

    first commit

---
+++ aeropuerto_api/catalog/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+class Gates(models.Model):
+    code = models.CharField(max_length=10, unique=True)
+    terminal = models.CharField(max_length=20, unique=True)
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(null=True, blank=True)      
+
+
+    def __str__(self):
+        return self.code
+
+
+
+class Estado(models.TextChoices):
+    SCHEDULED = "scheduled", "Scheduled"
+    BOARDING = "boarding", "Boarding"
+    DEPARTED = "departed", "Departed"
+    DELAYED = "delayed", "Delayed"
+    CANCELLED = "cancelled", "Cancelled"
+
+
+class Flights(models.Model):
+    gate = models.ForeignKey(Gates, on_delete=models.PROTECT, related_name="gates")
+    flight_number = models.CharField(max_length=20)
+    destination = models.CharField(max_length=100)
+    status = models.CharField(
+        max_length=20,
+        choices=Estado.choices,
+        default=Estado.SCHEDULED
+    )
+    departure_time = models.TimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.gate.code} {self.flight_number} ({self.destination})"
+
+
+
+
